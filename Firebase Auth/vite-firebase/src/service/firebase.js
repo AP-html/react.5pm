8c7710109d8729a022670dc8bdc5ad8492c8f66b
AP-html <ajaypatil5017@gmail.com>
2024-07-11 21:01:21 +0530

    update

---
+++ Firebase Auth/vite-firebase/src/service/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// import {getauth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBo_RnECzTWok4_U75-Nd-GpeQQ2kvUDCQ",
+  authDomain: "auth-1cdf5.firebaseapp.com",
+  projectId: "auth-1cdf5",
+  storageBucket: "auth-1cdf5.appspot.com",
+  messagingSenderId: "576502315592",
+  appId: "1:576502315592:web:6b675db5cb6291375b767d",
+  measurementId: "G-BVQE9ST7LS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app)
+const analytics = getAnalytics(app);
